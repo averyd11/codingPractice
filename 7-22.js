1d447ccb300858fd9f4a28f37c4ff44be6e432c2
adoyle11 <averyjdoyle@gmail.com>
2022-06-22 15:59:59 -0400

    added practice function to arrow function

---
+++ 7-22.js
@@ -0,0 +1,17 @@
+// convert these functions to arrow functions
+
+function sum(a,b) {
+    return a + b;
+}
+
+function isPositive(number) {
+    return number >= 0;
+}
+
+function randomNumber( {
+    return Math.random;
+})
+
+document.addEventListerner('click', function() {
+    console.log('click');
+})
